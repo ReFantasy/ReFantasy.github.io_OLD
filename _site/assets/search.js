ca39b02869b6e3074913eb6f262c7a4e9674207f
ReFantasy <309741309@qq.com>
2020-03-06 02:29:48 +0800

    修改 about

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"随机事件与概率",'url':"/2020/03/04/%E9%9A%8F%E6%9C%BA%E4%BA%8B%E4%BB%B6%E4%B8%8E%E6%A6%82%E7%8E%87.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"随机事件与概率",'url':"/2020/03/04/%E9%9A%8F%E6%9C%BA%E4%BA%8B%E4%BB%B6%E4%B8%8E%E6%A6%82%E7%8E%87.html"}]};
